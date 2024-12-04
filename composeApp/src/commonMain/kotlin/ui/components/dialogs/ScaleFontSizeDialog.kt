987c5554cec66010f0ed835cbb766a93e9006be7
mrfatworm <mrfatworm@gmail.com>
2024-12-04 14:00:50 +0800

    refactor: Breaking Down ScaleFontSizeDialog

--- composeApp/src/commonMain/kotlin/ui/components/dialogs/ScaleFontSizeDialog.kt
+++ composeApp/src/commonMain/kotlin/ui/components/dialogs/ScaleFontSizeDialog.kt
@@ -9,11 +9,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -48,61 +46,23 @@ fun ScaleFontSizeDialog(
 ) {
     var uiScale by AppTheme.uiScale
     var fontScale by AppTheme.fontScale
-    BasicDialog(
-        modifier = modifier, onDismissRequest = {
-            uiScale = uiScaleValue
-            fontScale = fontScaleValue
-            onDismiss()
-        }) {
+    BasicDialog(modifier = modifier, onDismissRequest = {
+        uiScale = uiScaleValue
+        fontScale = fontScaleValue
+        onDismiss()
+    }) {
         Column(
             modifier = Modifier.padding(
                 start = 32.dp, top = 32.dp, end = 32.dp, bottom = 16.dp
-            ), verticalArrangement = Arrangement.spacedBy(16.dp)
+            ), verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
-            Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                RarityItem(name = "", placeHolder = {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        painter = painterResource(Res.drawable.img_summer_solstice_2023_artwork),
-                        contentDescription = null
-                    )
-                })
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = stringResource(Res.string.ui_scale_warning),
-                    style = AppTheme.typography.bodyMedium
-                )
-            }
-            Text(
-                text = stringResource(Res.string.ui_scale) + " " + stringResource(Res.string.multiply) + ": ${
-                    round(
-                        uiScale * 10
-                    ) / 10
-                }x"
-            )
-            ZzzSlider(
-                modifier = Modifier.fillMaxWidth(),
-                value = uiScale,
-                onValueChange = { uiScale = it },
-                valueRange = 0.5f..1.5f,
-                steps = 9
-            )
-            Spacer(Modifier.size(8.dp))
-            Text(
-                text = stringResource(Res.string.font_scale) + " " + stringResource(Res.string.multiply) + ": ${
-                    round(
-                        fontScale * 10
-                    ) / 10
-                }x"
-            )
-            ZzzSlider(
-                modifier = Modifier.fillMaxWidth(),
-                value = fontScale,
-                onValueChange = { fontScale = it },
-                valueRange = 0.5f..1.5f,
-                steps = 9,
-            )
-            Spacer(Modifier.size(8.dp))
+            ScalePreview()
+            ScaleUiSlider(stringResource(Res.string.ui_scale), uiScale, onScaleValueChange = {
+                uiScale = it
+            })
+            ScaleUiSlider(stringResource(Res.string.font_scale), fontScale, onScaleValueChange = {
+                fontScale = it
+            })
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End)
@@ -117,5 +77,42 @@ fun ScaleFontSizeDialog(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun ScalePreview() {
+    Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        RarityItem(name = "", placeHolder = {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(Res.drawable.img_summer_solstice_2023_artwork),
+                contentDescription = null
+            )
+        })
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(Res.string.ui_scale_warning),
+            style = AppTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Composable
+private fun ScaleUiSlider(
+    scaleType: String, scaleValue: Float, onScaleValueChange: (Float) -> Unit
+) {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        val firstDigitPlace = round(scaleValue * 10) / 10
+        Text(
+            text = "$scaleType ${stringResource(Res.string.multiply)}: ${firstDigitPlace}x"
+        )
+        ZzzSlider(
+            modifier = Modifier.fillMaxWidth(),
+            value = scaleValue,
+            onValueChange = onScaleValueChange,
+            valueRange = 0.5f..1.5f,
+            steps = 9
+        )
     }
 }
